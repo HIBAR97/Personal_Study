d02fa24ec2a59f91197429979d8da160e543b2dc
HIBER97 <hibar9710@gmail.com>
2023-02-10 11:29:24 +0900

    2023. 2. 10. AM 11:29 --------------------------------------- ADD - 튜플까지 복습 - --------------------------------------- Delete - - --------------------------------------- Error - - --------------------------------------- Fix - - ---------------------------------------

---
+++ Hello world.swift
@@ -0,0 +1,22 @@
+//
+//  Hello world.swift
+//  Personal_Study
+//
+//  Created by Kim Andrew on 2023/02/09.
+//
+
+import Foundation
+
+//변수 설정
+var name : String
+var age : Int
+var pi : Double
+
+//상수 설정
+let greeting = "hello world"
+
+//데이터 구조
+let (Name, Age) = data_set1
+data_set1 = (Name : kim, Age : 10)
+print("dateset 1 \(data_set1.0) and \(data_set1.1)")
+
