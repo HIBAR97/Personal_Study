3379da4d93c26c1a7c681b1b9b0150a346fd4a61
HIBER97 <hibar9710@gmail.com>
2023-02-11 12:55:53 +0900

    2023. 2. 11. PM 12:55 --------------------------------------- ADD - 튜플 공부중 - --------------------------------------- Delete - - --------------------------------------- Error - - --------------------------------------- Fix - - ---------------------------------------

--- Hello world.swift
+++ Hello world.swift
@@ -15,8 +15,16 @@ var pi : Double
 //상수 설정
 let greeting = "hello world"
 
-//데이터 구조
+//데이터 구조 (튜플)
 let (Name, Age) = data_set1
 data_set1 = (Name : kim, Age : 10)
 print("dateset 1 \(data_set1.0) and \(data_set1.1)")
+
+let httpError = (404, "Not Found")
+let (Code, Message) = httpError
+print("Status \(Code) message \(Message)")
+
+//튜플를 모를경우 바뀔경우
+let (justCode, _) = httpError
+print("Status \(justCode)")
 
