8f9dfd115c02394590903fe1ab50af76a84669ee
Andy <96377739+HIBAR97@users.noreply.github.com>
2023-04-06 15:27:33 +0900

    Update Hello world.swift

--- Hello world.swift
+++ Hello world.swift
@@ -447,4 +447,20 @@ func stepBackward(_ input: Int) -> Int {
 }
 //부울 매개변수를 기반으로 함수의 값을 반환함
 
+//중첩 함수
+func chooseStepFunction(backward: Bool) -> (Int) -> Int {
+    func stepForward(input: Int) -> Int { return input + 1 }
+    func stepBackward(input: Int) -> Int { return input - 1 }
+    return backward ? stepBackward : stepForward
+}
+var currentValue = -4
+let moveNearerToZero = chooseStepFunction(backward: currentValue > 0)
+// moveNearerToZero now refers to the nested stepForward() function
+while currentValue != 0 {
+    print("\(currentValue)... ")
+    currentValue = moveNearerToZero(currentValue)
+}
+print("zero!")
+
+//밖에서는 1개의 함수로 보이지만 안 속은 여러개의 함수로 이루어짐
 
