0acdfcfe9f3a452bb7ff1f503c0e693e3a55fdb3
Andy <96377739+HIBAR97@users.noreply.github.com>
2023-06-08 21:33:53 +0900

    Update Hello world.swift

--- Hello world.swift
+++ Hello world.swift
@@ -831,3 +831,16 @@ let sum = ArithmeticExpression.addition(five, four)
 let product = ArithmeticExpression.multiplication(sum, ArithmeticExpression.number(2))
 
 //간단한 방법
+func evaluate(_ expression: ArithmeticExpression) -> Int {
+    switch expression {
+    case let .number(value):
+        return value
+    case let .addition(left, right):
+        return evaluate(left) + evaluate(right)
+    case let .multiplication(left, right):
+        return evaluate(left) * evaluate(right)
+    }
+}
+
+
+print(evaluate(product))
