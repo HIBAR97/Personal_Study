a3ca743fd129fbb6d925dffbbf4c4404a42b6749
Andy <96377739+HIBAR97@users.noreply.github.com>
2023-06-25 14:04:29 +0900

    Update Hello world.swift

--- Hello world.swift
+++ Hello world.swift
@@ -1006,7 +1006,38 @@ class DataManager {
     // the DataManager class would provide data management functionality here
 }
 
+//지속된 속성 및 인스턴스 변수
+//이러한 개념을 단일 속성 선언으로 통합
 
+//계산된 속성
+struct Point {
+    var x = 0.0, y = 0.0
+}
+struct Size {
+    var width = 0.0, height = 0.0
+}
+struct Rect {
+    var origin = Point()
+    var size = Size()
+    var center: Point {
+        get {
+            let centerX = origin.x + (size.width / 2)
+            let centerY = origin.y + (size.height / 2)
+            return Point(x: centerX, y: centerY)
+        }
+        set(newCenter) {
+            origin.x = newCenter.x - (size.width / 2)
+            origin.y = newCenter.y - (size.height / 2)
+        }
+    }
+}
+var square = Rect(origin: Point(x: 0.0, y: 0.0),
+    size: Size(width: 10.0, height: 10.0))
+let initialSquareCenter = square.center
+// initialSquareCenter is at (5.0, 5.0)
+square.center = Point(x: 15.0, y: 15.0)
+print("square.origin is now at (\(square.origin.x), \(square.origin.y))")
+// Prints "square.origin is now at (10.0, 10.0)"
 let manager = DataManager()
 manager.data.append("Some data")
 manager.data.append("Some more data")
