56494bac26e9603b3d8aaad06ed8afaba8999ae2
Andy <96377739+HIBAR97@users.noreply.github.com>
2023-07-03 19:32:09 +0900

    Update Hello world.swift

--- Hello world.swift
+++ Hello world.swift
@@ -1149,4 +1149,32 @@ struct SmallRectangle {
 }
 
 //속성에 대한 getter 및 setter 및 래핑 액세스
-	
+
+//래핑된 속성의 초기값 설정
+@propertyWrapper
+struct SmallNumber {
+    private var maximum: Int
+    private var number: Int
+
+
+    var wrappedValue: Int {
+        get { return number }
+        set { number = min(newValue, maximum) }
+    }
+
+
+    init() {
+        maximum = 12
+        number = 0
+    }
+    init(wrappedValue: Int) {
+        maximum = 12
+        number = min(wrappedValue, maximum)
+    }
+    init(wrappedValue: Int, maximum: Int) {
+        self.maximum = maximum
+        number = min(wrappedValue, maximum)
+    }
+}
+
+//래핑된 속성에 대해 다른 초기 값을 지정할 수 없습니다
